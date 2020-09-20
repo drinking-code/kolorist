fa16722be8aa436a862233d92d3d33bfc9c87a54
Johannes Wipper <joe.wipper@gmx.de>
2020-09-20 15:27:48 +0200

    new approach: basics are working
    flawless

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-const {loadWASM, OnigRegExp} = require('onigasm')
+const {loadWASM, OnigScanner} = require('onigasm')
 
 let kolorist = {};
 
-kolorist.init = async function (grammar) {
+kolorist.init = async function (grammar) { // todo: init multiple grammars
     switch (grammar.toLowerCase()) {
         case 'javascript':
             grammar = 'https://cdn.jsdelivr.net/gh/textmate/javascript.tmbundle@master/Syntaxes/JavaScript.plist'
     }
 
-    new Promise((resolve, reject) => {
-
+    return new Promise((resolve, reject) => {
         let xmlHttp = new XMLHttpRequest(), plist;
         xmlHttp.onreadystatechange = () => {
             if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
                 plist = xmlHttp.response;
-                plist = plist.replace(/\n/g, '').replace(/\t/g, '')
+                plist = plist.replace(/(?:\t)#[\S\s]+?(?=\n)/g, '').replace(/[\n\t]/g, '')
                 let parser, plistParsed;
                 if (window.DOMParser) {
                     parser = new DOMParser();
@@ -25,7 +24,12 @@ kolorist.init = async function (grammar) {
                     plistParsed.async = false;
                     plistParsed.loadXML(plist);
                 }
-                translate(plistParsed, resolve)
+
+                console.log(plistParsed)
+                translate(plistParsed)
+                    .then(j => makeKoloristGrammars(j))
+                    .then(g => resolve(g))
+
             } else if (xmlHttp.readyState === 4) {
                 reject()
             }
@@ -35,118 +39,129 @@ kolorist.init = async function (grammar) {
 
     })
 
-    function translate(xml, resolve) {
-        // console.log(xml)
-        function generateProperties(xml) {
-            let json = {};
-            // console.log(xml)
-            // iterate every given child
-            for (let i = 0; i < xml.children.length; i++) {
-                const elm = xml.children[i];
-                if (elm.tagName !== 'key') continue // iterate only <key> elements (and their associated sibling)
-                // if sibling is an array
-                if (elm.nextSibling.tagName === 'array') {
-                    let array = []
-                    for (let i = 0; i < elm.nextSibling.children.length; i++) {
-                        const item = elm.nextSibling.children[i]
-                        // if item has children (<dict>)
-                        if (item.children.length !== 0)
-                            array.push(generateProperties(item))
-                        else // if item has no children (<string>)
-                            array.push(item.textContent)
-                    }
-                    json[elm.textContent] = array;
-                } else if (elm.nextSibling.children.length !== 0) // if sibling has children but is not an array
-                    json[elm.textContent] = generateProperties(elm.nextSibling)
-                else // if sibling has no children
-                    json[elm.textContent] = elm.nextSibling.textContent;
+    // translate from plist xml to json
+    function translate(xml) {
+        return new Promise(resolve => {
+            function generateProperties(xml) {
+                let json = {};
+                // iterate every given child
+                for (let i = 0; i < xml.children.length; i++) {
+                    const elm = xml.children[i];
+                    if (elm.tagName !== 'key') continue // iterate only <key> elements (and their associated sibling)
+                    // if sibling is an array
+                    if (elm.nextSibling.tagName === 'array') {
+                        let array = []
+                        for (let i = 0; i < elm.nextSibling.children.length; i++) {
+                            const item = elm.nextSibling.children[i]
+                            // if item has children (<dict>)
+                            if (item.children.length !== 0)
+                                array.push(generateProperties(item))
+                            else // if item has no children (<string>)
+                                array.push(item.textContent)
+                        }
+                        json[elm.textContent] = array;
+                    } else if (elm.nextSibling.children.length !== 0) // if sibling has children but is not an array
+                        json[elm.textContent] = generateProperties(elm.nextSibling)
+                    else // if sibling has no children
+                        json[elm.textContent] = elm.nextSibling.textContent;
+                }
+                return json
             }
-            return json
-        }
 
-        kolorist.grammar = generateProperties(xml.querySelector('dict'))
-        resolve()
+            const grammar = generateProperties(xml.querySelector('dict'))
+            resolve(grammar)
+        })
     }
+
+    // sort pattern contents into usable arrays
+    function makeKoloristGrammars(json) {
+        return new Promise(resolve => {
+            let patterns = [], names = [], endPatterns = {}, patternsPatterns = {}
+            json.patterns.forEach((pattern, index) => {
+                let namesCaptures = {
+                    name: (pattern.name) ? pattern.name : undefined
+                }
+                if (pattern.match) {
+                    patterns.push(pattern.match.replace(/\s/g, ''))
+                    if (pattern.captures) {
+                        namesCaptures.captures = {}
+                        for (let i in pattern.captures) {
+                            if (!pattern.captures.hasOwnProperty(i)) continue
+                            namesCaptures.captures[i.toString()] = pattern.captures[i].name
+                        }
+                    }
+                } else if (pattern.begin && pattern.end) {
+                patterns.push(pattern.begin.replace(/\s/g, ''))
+                    if (pattern.beginCaptures) {
+                        namesCaptures.beginCaptures = {}
+                        for (let i in pattern.beginCaptures) {
+                            if (!pattern.beginCaptures.hasOwnProperty(i)) continue
+                            namesCaptures.beginCaptures[i.toString()] = pattern.beginCaptures[i].name
+                        }
+                    }
+                    if (pattern.endCaptures) {
+                        namesCaptures.endCaptures = {}
+                        for (let i in pattern.endCaptures) {
+                            if (!pattern.endCaptures.hasOwnProperty(i)) continue
+                            namesCaptures.endCaptures[i.toString()] = pattern.endCaptures[i].name
+                        }
+                    }
+                    endPatterns[index] = pattern.end
+                    if (pattern.patterns) {
+                        // todo (same as include +specifically defined patterns)
+                    }
+                } else if (pattern.include) {
+                    // todo
+                }
+                names.push(namesCaptures)
+            })
+            resolve({patterns, names, endPatterns, patternsPatterns})
+        })
+    }
+
+    /* parsed grammar:
+    * 2 arrays and 2 objects
+    * - array: all patterns
+    *       (with #include patterns, begin/end only begin)
+    * - array: all names for the pattern
+    *       (every item is an object with 2 keys: name <name of pattern or undefined>, and captures <defined captures>)
+    * - object: all end patterns (key: index of begin/end pattern in first array <end pattern>)
+    * - object: all names for end patterns (key: index of end pattern in first array <name>)
+    * - object: all patterns of begin/end patterns todo: is good? maybe repo lookup table?
+    *       (key: index of begin/end pattern in first array <object of all included patterns>)*/
+
 }
 
-kolorist.highlight = async function (code) {
+kolorist.highlight = async function (code, grammar) {
     // initiate oniguruma engine
     await loadWASM('./node_modules/onigasm/lib/onigasm.wasm')
-    // splice for strings -> inserting strings into lines
-    String.prototype.splice = function (idx, rem, str) {
-        return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
-    };
-    // split code into lines
-    code = code.split('\n')
-    let html = '<pre>';
-    code.forEach((line, i) => {
-        // if (i !== 9) return
-        // replace < character to prevent creating tags when writing to the document later
-        line = line.replace('<', '&lt;')
-        // iterate every grammar rule (pattern)
-        let matches = [];
-        for (let i = 0; i < kolorist.grammar.patterns.length; i++) {
-            const pattern = kolorist.grammar.patterns[i];
-            if (pattern.match) { // simple matches
-                const regex = new OnigRegExp(pattern.match)
-                // match all in line
-                let match = regex.searchSync(line)
-                if (match) {
-                    matches.push({match, name: pattern.name, captureNames: pattern.captures})
-                    // console.log(match, pattern.name, pattern.comment, pattern.match, line)
-                }
-            } else if (pattern.begin && pattern.end) { // enclosing matches (with begin and end)
-                // TODO: also match nested patterns
-                const regex1 = new OnigRegExp(pattern.begin)
-                let matchBegin = regex1.searchSync(line)
-                const regex2 = new OnigRegExp(pattern.begin)
-                let matchEnd = regex2.searchSync(line)
-                if (matchBegin) {
-                    matches.push({matchBegin, name: pattern.name, captureNames: pattern.captures})
-                    // console.log(match, pattern.name, pattern.comment, pattern.match, line)
-                }
-            }
-            // including patterns in grammar's repository
-        }
-        // add classes to every match
-        for (let i = 0; i < matches.length; i++) {
-            // console.log(line);
-            const match = matches[i],
-                capture = match.match[0],
-                tag = `<span class="${(match.name) ? match.name.replace('.', '-') : 'undefined-code'}">`
-            // wrapping spans around capture (below referred to as ^ capture) //TODO: also wrap named capture groups
-            line = line.splice(capture.end, 0, `</span>`)
-            line = line.splice(capture.start, 0, tag)
-            if (matches.length - 1 === i) continue
-            // shift index numbers remaining for the line
-            for (let j = i; j < matches.length; j++) {
-                // every capture in the match
-                matches[j].match.forEach(thisCapture => {
-                    /* TODO: rework here, probably doesn't cover all cases
-                    * intersection1: cap 1 starts before cap 2 starts and ends before cap 2 ends but after cap 2 starts
-                    * intersection1: cap 1 starts after cap 2 starts but before cap 2 ends and ends after cap 2 ends
-                     */
-                    // only if this capture is after the ^ capture in the string
-                    if (thisCapture.start > capture.start) {
-                        const tagLength = tag.length
-                        // case 1: this capture is completely after the ^ capture
-                        if (thisCapture.start > capture.start + capture.length)
-                            thisCapture.start += tagLength + 7
-                        else // case 2: this capture is inside the ^ capture
-                            thisCapture.start += tagLength
-                        thisCapture.end += tagLength + 7
-                        // if this capture starts with
-                    } else if (thisCapture.start === capture.start) {
-                        thisCapture.end += tag.length + capture.length + 7
-                    }
-                })
-            }
-        }
 
-        html += `<div>${line}</div>`
-        // console.log(line)
-    })
-    html += '</pre>'
+    const scanner = new OnigScanner(grammar.patterns)
+    let html = []
+
+    console.log(grammar.patterns[4])
+
+    function scanForMatch(pos) {
+        // find next match (from pos)
+        let match = scanner.findNextMatch(code, pos, (error, match) => {
+            if (error) console.error(error)
+            console.log(match)
+        })
+        /*if (!match) return
+        console.log(match)
+        scanForMatch(match.end)*/
+        // write everything not matched (from pos to match.start) to array as plain text
+        // check if enclosed pattern ("begin", not "match" keyword)
+        // ->   recursive function call (with in grammar defined #include patterns)
+        // then write match with
+    }
+
+    scanForMatch(0)
+
+    function scanInside(pos, index) {
+
+    }
+
     return html
 }
 
